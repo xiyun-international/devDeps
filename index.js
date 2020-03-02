f99badacf348dd75ad05bbb3fedb110e70d0b180
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2020-03-02 18:53:47 +0800

    fix: 修改提示，去掉死循环

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const chalk = require("chalk");
 const { join } = require("path");
 
 const devDeps = function(cwd) {
-  console.log(chalk.green('即将安装 devDependencies，只支持 Yarn 工具。'));
   const path = cwd || process.cwd();
+  const nodeModulesPath = join(path, "node_modules")
+
+  console.log(chalk.green('即将安装 devDependencies，只支持 Yarn 工具。'));
+  console.log(chalk.green(`安装目录为：${nodeModulesPath}`));
+
   const subprocess = execa("yarn", [
     "install",
     "--modules-folder",
@@ -12,6 +16,5 @@ const devDeps = function(cwd) {
   ]);
   subprocess.stdout.pipe(process.stdout);
 };
-devDeps();
 
 module.exports = devDeps;
